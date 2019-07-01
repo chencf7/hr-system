36d11d1d11a2e7c80461d86068d22bd47ec4f805
chenchaofan <chen_chaofan@hotmail.com>
2019-07-01 21:28:22 +0800

    add homepage

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface IProps{
+
+}
+
+interface IState{
+
+}
+
+class HomeComp extends React.Component<IProps, IState>{
+  render(){
+    return (
+      <div>
+        123
+      </div>
+    );
+  }
+}
+
+export default HomeComp;
--- src/routerconfig/Router.tsx
+++ src/routerconfig/Router.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Brouter, Switch, Route} from 'react-router-dom';
+
 import Login from 'pages/home/login';
+import Home from 'pages/home/home';
 
 export default ()=>{
   return (
     <Brouter>
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/home" component={Home} />
       </Switch>
     </Brouter>
   );
