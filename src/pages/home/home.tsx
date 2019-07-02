7df80d8888e4aa0c6ac8d3434524c233d6a76422
chenchaofan <chen_chaofan@hotmail.com>
2019-07-02 20:18:32 +0800

    更新

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -18,7 +18,7 @@ class Homecomp extends React.Component<IProps, IState>{
         <Navbar></Navbar>
         <div className="hr-menu">
           <nav>
-            <ul className="menu-group">
+            <ul className="menu-group clear">
               <li className="menu-item">
                 <a>
                   <svg className="icon" aria-hidden="true">
@@ -26,11 +26,25 @@ class Homecomp extends React.Component<IProps, IState>{
                   </svg>
                   组织管理
                 </a>
-                <div></div>
+                <div className="menu-item_in" >
+                  <dl className="fr">
+                      <dd>组织机构管理</dd>
+                      <dd>人员调动管理</dd>
+                      <dd>密码信息重置</dd>
+                    </dl>
+                </div>
               </li>
               <li className="menu-item">
                 <a>考勤管理</a>
-                <div></div>
+                <div className="menu-item_in" >
+                  <div className="wrap">
+                      <dl className="fr">
+                        <dd>1</dd>
+                        <dd>2</dd>
+                        <dd>3</dd>
+                      </dl>
+                  </div>
+                </div>
               </li>
             </ul>
           </nav>
